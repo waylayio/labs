2003ef9424f14a51944f49583d9fb5556ef4d287
pizurican <pizurica@gmail.com>
2015-04-06 20:28:15 +0200

    Revert colors back to white

--- site/js/main.js
+++ site/js/main.js
@@ -305,3 +305,17 @@ $(document).ready(function(){
         });
 
 });
+
+
+$('#chooseFile').bind('change', function () {
+  var filename = $("#chooseFile").val();
+  if (/^\s*$/.test(filename)) {
+    $(".file-upload").removeClass('active');
+    $("#noFile").text("No file chosen..."); 
+  }
+  else {
+    $(".file-upload").addClass('active');
+    $("#noFile").text(filename.replace("C:\\fakepath\\", "")); 
+  }
+});
+
